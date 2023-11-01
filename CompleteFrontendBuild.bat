a7e860016d8632e11269e8aa77d19676323f0fce
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2023-11-01 13:09:38 -0500

    InitialPush

---
+++ CompleteFrontendBuild.bat
@@ -0,0 +1,8 @@
+cd F:\Omniscape\Omniscape\ModifiedSamples\Samples\PixelStreaming\WebServers\Frontend\ui-library
+call npm install
+call npm run build
+
+cd F:\Omniscape\Omniscape\ModifiedSamples\Samples\PixelStreaming\WebServers\SignallingWebServer\platform_scripts\cmd
+call build.bat --build
+
+Pause
