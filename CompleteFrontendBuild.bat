d1aea79ef3d96a34a7b4d63b45bfa28943a9f253
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2023-11-02 14:50:01 -0500

    .
    Removed epics implementation to make the size smaller.

--- CompleteFrontendBuild.bat
+++ CompleteFrontendBuild.bat
@@ -1,8 +1,10 @@
-cd F:\Omniscape\Omniscape\ModifiedSamples\Samples\PixelStreaming\WebServers\Frontend\ui-library
+cd F:\Omniscape\Omniscape\ModifiedSamples\ModifiedSamples\Samples\PixelStreaming\WebServers\Frontend\ui-library
 call npm install
+call npm install @4players/odin
+call npm install @4players/odin-tokens
 call npm run build
 
-cd F:\Omniscape\Omniscape\ModifiedSamples\Samples\PixelStreaming\WebServers\SignallingWebServer\platform_scripts\cmd
+cd F:\Omniscape\Omniscape\ModifiedSamples\ModifiedSamples\Samples\PixelStreaming\WebServers\SignallingWebServer\platform_scripts\cmd
 call build.bat --build
 
 Pause
